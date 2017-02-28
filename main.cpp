0e4e3f51a87ef77d67366d254290a4d2aa72a8a2
Mario Geiger <geiger.mario@gmail.com>
2017-02-28 10:20:37 +0100

    clearer example

--- main.cpp
+++ main.cpp
@@ -6,46 +6,44 @@
 
 class Sinusoidal : public XYFunction {
 public :
-    qreal y(qreal x)
-    {
-        return std::pow(std::sin(x),3);
-    }
+	qreal y(qreal x)
+	{
+		return std::pow(std::sin(x),3);
+	}
 
 };
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+	QApplication a(argc, argv);
 
-    Sinusoidal sinus;
-
-    // crée la vue
-    XYGraph graph;
-
-    // crée la scène
-    XYScene scene;
-
-    scene.setBackgroundBrush(QBrush(Qt::white));
-    scene.setSubaxesPen(Qt::NoPen);
-    scene.setAxesPen(QPen());
-    scene.setZoomPen(QPen(Qt::darkGreen));
-    scene.setTextColor(QColor("red"));
-
+	XYScene scene;
+	scene.setBackgroundBrush(QBrush(Qt::white));
+	scene.setSubaxesPen(Qt::NoPen);
+	scene.setAxesPen(QPen());
+	scene.setZoomPen(QPen(Qt::darkGreen));
+	scene.setTextColor(QColor("red"));
 	scene.setFlag(XYScene::ShowPointPosition);
 
-	XYScatterplot points(QList<QPointF>() << QPointF(0.0, 0.0) << QPointF(0.5, 1.0) << QPointF(1.0, 1.0) << QPointF(1.045, 1.05), QPen(), QBrush(), 5.0, QPen(Qt::red));
-	scene.addScatterplot(&points);
+	// add scatter points
+	QList<QPointF> points;
+	points << QPointF(0.0, 0.0) << QPointF(0.5, 1.0) << QPointF(1.0, 1.0) << QPointF(1.045, 1.05);
 
-    scene.addFunction(&sinus);
+	XYScatterplot my_scatterplot(points, QPen(), QBrush(), 5.0, QPen(Qt::red));
+	scene.addScatterplot(&my_scatterplot);
 
-    scene.autoZoom();
-    scene.relativeZoom(1.2);
+	// add function
+	Sinusoidal sinus;
+	scene.addFunction(&sinus);
 
-    // donne la scène à la vue
-    graph.setScene(&scene);
-    graph.setWindowTitle("Test 1");
-    graph.show();
-    graph.resize(600, 600);
+	scene.autoZoom();
+	scene.relativeZoom(1.2);
 
-    return a.exec();
+	// create the view
+	XYGraph graph(&scene);
+	graph.setWindowTitle("Test 1");
+	graph.show();
+	graph.resize(600, 600);
+
+	return a.exec();
 }
